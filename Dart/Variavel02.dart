c87ca8cc8662c74aa4c6a551261a06e6daf77b55
Thalles Anderson <thallesanderson@hotmail.com>
2025-07-14 03:39:11 -0300

    Update Variavel02.dart

--- Dart/Variavel02.dart
+++ Dart/Variavel02.dart
@@ -1,4 +1,21 @@
 main(){
 
-  print("Teste");
+  var A = 2;
+  var B = 2.83;
+  var C = true;
+  var D = "Soma: ";
+
+  //print(D + (A + B).toString());
+
+  print(A.runtimeType);
+  print(B.runtimeType);
+  print(C.runtimeType);
+  print(D.runtimeType);
+
+  print(A is int);
+  print(B is double);
+  print(C is bool);
+  print(D is String);
+
+
 }
